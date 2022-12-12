7c9755f2ea24bdadada99fc4938a3710cbe83ea8
LuciusEg <32649006+LuciusEg@users.noreply.github.com>
2022-12-12 11:01:34 +0300

    another commit

---
+++ documentation/client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public forecasts?: WeatherForecast[];
+  _http: HttpClient;
+
+  constructor(http: HttpClient) {
+    this._http = http;
+    this.refreshForecast();
+  }
+
+  title = 'client';
+
+  refreshForecast(){
+    this._http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
