16d2716a663ff141ea5b8a9cab798c63f7c00401
LuciusEg <32649006+LuciusEg@users.noreply.github.com>
2022-12-12 12:57:49 +0300

    weatherCountCHanged

--- documentation/documentation/Controllers/WeatherForecastController.cs
+++ documentation/documentation/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace documentation.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 50).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
