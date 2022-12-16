94c1fb4b21a98e7fbda31b1b6c3a9f3f7d1668ec
Мухаммад <90333838+Dungeon-master1@users.noreply.github.com>
2022-12-16 13:50:56 +0300

    Table (#17)
    * Name
    
    * Добавил Angular Material
    
    * Добавил таблицу
    
    Co-authored-by: Maxim <89983687+MaximZXC24@users.noreply.github.com>
    Co-authored-by: Vladislav Egorov <32649006+LuciusEg@users.noreply.github.com>

--- documentation/client/src/app/app.module.ts
+++ documentation/client/src/app/app.module.ts
@@ -5,13 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MaterialModule } from 'src/material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatSlideToggleModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatSlideToggleModule, MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
