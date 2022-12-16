2e824cb5f2c53692ee89c75eaeed8f8005a1995e
Maxim <89983687+MaximZXC24@users.noreply.github.com>
2022-12-16 12:53:34 +0300

    Material (#14)
    * Name
    
    * Добавил Angular Material

--- documentation/client/src/app/app.module.ts
+++ documentation/client/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
