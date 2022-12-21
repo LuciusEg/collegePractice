76076f31e404904e0bb9a1a5df9168a2aab2966e
Иван Ханин <Dazzmenator@gmail.com>
2022-12-21 13:06:32 +0300

    Document Controller

--- documentation/documentation/Controllers/DocumentsController.cs
+++ documentation/documentation/Controllers/DocumentsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using documentation;
 using documentation.Models;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 
 namespace documentation.Controllers
 {
@@ -20,14 +21,114 @@ namespace documentation.Controllers
         {
             _context = context;
         }
-
-
+        //не забыть поменять подключение к бд
         //Продолжить работу тут
-        // GET: api/Documents
+        // GET: api/Users
+        private static Document DocumentTO(Document document) =>
+            new Document
+            {
+                Id = document.Id,
+                Title = document.Title,
+                Description = document.Description,
+                Number = document.Number,
+                Author = document.Author,
+                Recipient = document.Recipient
+            };
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Document>>> GetDocuments()
         {
             return BadRequest();
+        }
+
+        //вывод всех пользователей, работает
+        [HttpGet("allDocuments")]
+        public async Task<ActionResult<IEnumerable<Document>>> allDocuments()
+        {
+            return await _context.Documents.ToListAsync();
+        }
+
+        //работает
+        [HttpGet("oneDocument")]
+        public async Task<ActionResult<IEnumerable<Document>>> oneDocument(int id)
+        {
+            return await _context.Documents
+               .Select(x => DocumentTO(x))
+               .ToListAsync();
+        }
+
+        [HttpPost("createDocuments")]
+        public async Task<ActionResult<IEnumerable<Document>>> createUsers(Document documentTO)
+        {
+            var document = new Document
+            {
+                Id = documentTO.Id,
+                Title = documentTO.Title,
+                Description = documentTO.Description,
+                Number = documentTO.Number,
+                Author = documentTO.Author,
+                Recipient = documentTO.Recipient
+            };
+
+            _context.Documents.Add(document);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetDocuments),
+                new { id = document.Id },
+                DocumentTO(document));
+        }
+        //работает
+        [HttpPut("UpdateDocuments")]
+        public async Task<ActionResult<IEnumerable<Document>>> UpdateDocuments(int id, [Bind("Id,Title,Description,Number,Author,Recipient")] Document document)
+        {
+            if (id != document.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(document);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DocumentExist(document.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return NoContent();
+        }
+
+        //работает
+        [HttpDelete("DeleteDocuments")]
+        public async Task<ActionResult<IEnumerable<Document>>> DeleteDocuments(int id)
+        {
+            var document = await _context.Documents.FindAsync(id);
+
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            _context.Documents.Remove(document);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DocumentExist(long id)
+        {
+            return _context.Documents.Any(e => e.Id == id);
         }
     }
 }
--- documentation/documentation/Controllers/UsersController.cs
+++ documentation/documentation/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using documentation;
 using documentation.Models;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 
 namespace documentation.Controllers
 {
@@ -20,7 +21,7 @@ namespace documentation.Controllers
         {
             _context = context;
         }
-
+        //не забыть поменять подключение к бд
         //Продолжить работу тут
         // GET: api/Users
         [HttpGet]
@@ -28,5 +29,108 @@ namespace documentation.Controllers
         {
             return BadRequest();
         }
+
+        //вывод всех пользователей, работает
+        [HttpGet("allUsers")]
+        public async Task<ActionResult<IEnumerable<User>>> allUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        //работает
+        [HttpGet("oneUsers")]
+        public async Task<ActionResult<IEnumerable<User>>> oneUsers(int id)
+        {
+            return await _context.Users
+               .Select(x => UserTO(x))
+               .ToListAsync();
+        }
+
+        [HttpPost("createUsers")]
+        public async Task<ActionResult<IEnumerable<User>>> createUsers(User userTO)
+        {
+            var user = new User
+            {
+                FirstName = userTO.FirstName,
+                LastName = userTO.LastName,
+                MiddleName = userTO.MiddleName,
+                Role = userTO.Role,
+                JobTitle = userTO.JobTitle,
+                Department = userTO.Department
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetUsers),
+                new { id = user.Id },
+                UserTO(user));
+        }
+        //работает
+        [HttpPut("UpdateUsers")]
+        public async Task<ActionResult<IEnumerable<User>>> UpdateUsers(int id, [Bind("Id,FirstName,LastName,MiddleName,Role,JobTitle,Department")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExist(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return NoContent();
+        }
+
+        //работает
+        [HttpDelete("DeleteUsers")]
+        public async Task<ActionResult<IEnumerable<User>>> DeleteUsers(int id)
+        {
+            var todoItem = await _context.Users.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExist(long id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
+        private static User UserTO(User user) =>
+            new User
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                MiddleName = user.MiddleName,
+                Role = user.Role,
+                JobTitle = user.JobTitle,
+                Department = user.Department
+            };
     }
 }
